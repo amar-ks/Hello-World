6d333dd499891c4d9228bb4e87765447f87f8869
amar-ks <meetamarkumar@gmail.com>
2021-06-15 14:57:12 +0530

    Adding for loop in java

--- HelloJava.java
+++ HelloJava.java
@@ -3,5 +3,8 @@ class HelloJava
   public static void main(string arg[])
   {
     system.out.println("Hello world from java!")
+    
+    for(int i = 0; i <= 2; i++)
+    	system.out.println("Nice to meet you!")
   }
 }
